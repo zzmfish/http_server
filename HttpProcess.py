a1979df8e8c66d9dfb44d485b21c6f90e58613e0
zzmfish <zzmfish@gmail.com>
2014-03-21 14:58:16 +0800

    simulate slow request

--- HttpProcess.py
+++ HttpProcess.py
@@ -1,9 +1,11 @@
 import os
 import multiprocessing
-import SimpleHTTPServer
 import SocketServer
 import signal
 import socket
+import urlparse
+import time
+from SimpleHTTPServer import SimpleHTTPRequestHandler
 
 http_process = None
 
@@ -16,6 +18,33 @@ http_process = None
 def term_handler(signum, frame):
   global http_process
   http_process.stopped = True
+
+class RequestHandler(SimpleHTTPRequestHandler):
+    def parse_query(self):
+        query = {}
+        path = self.path
+        p = path.find('?')
+        if p >= 0:
+            query = urlparse.parse_qs(path[p+1:])
+        return query
+
+    def handle_slow(self, query):
+        t = float(query['__slow'][0])
+        f = self.send_head()
+        if f:
+            content = f.read()
+            p = len(content) / 2
+            self.wfile.write(content[0 : p])
+            time.sleep(t)
+            self.wfile.write(content[p : ])
+            f.close()
+        
+    def do_GET(self):
+        query = self.parse_query()
+        if query.has_key('__slow'):
+            return self.handle_slow(query)
+            
+        return SimpleHTTPRequestHandler.do_GET(self)
 
 class HttpProcess(multiprocessing.Process):
   def __init__(self, root, host, port):
@@ -32,7 +61,7 @@ class HttpProcess(multiprocessing.Process):
   def run(self):
     log('HttpProcess.run')
     os.chdir(self.root)
-    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+    Handler = RequestHandler
     httpd = SocketServer.TCPServer((self.host, self.port), Handler, False)
     httpd.timeout = 1
     httpd.allow_reuse_address = True
